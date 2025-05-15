3884c186be5dbdfaee9f5257fcf901463c2860b0
nuhamintesfe <nuhamintesfaye026@gmail.com>
2025-05-15 16:51:19 +0300

    Create 1-batch_processing.py

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,21 @@
+import seed
+
+def stream_users_in_batches(batch_size):
+    offset = 0
+    while True:
+        connection = seed.connect_to_prodev()
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute(f"SELECT * FROM user_data LIMIT {batch_size} OFFSET {offset}")
+        batch = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        if not batch:
+            break
+        yield batch
+        offset += batch_size
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if user['age'] > 25:
+                print(user)
