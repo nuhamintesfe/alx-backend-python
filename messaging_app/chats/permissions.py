0ced62a0b9b941923dfed246f05718ba8f3f7489
Nuhamin Tesfaye <nuhamintesfaye026@gmail.com>
2025-06-07 20:18:06 +0300

    Add permissions and custom authentication logic

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,5 +1,6 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
-class IsParticipantOfConversation(BasePermission):
+class IsParticipantOfConversation(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user in obj.participants.all()
+
