5d1ade58709f633d083a041c05f626341df5201d
Nuhamin Tesfaye <nuhamintesfaye026@gmail.com>
2025-06-08 14:44:03 +0300

    Implement custom middleware for Django-Middleware-0x03 project

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -45,7 +45,7 @@ class OffensiveLanguageMiddleware:
         return self.get_response(request)
 
 
-class RolePermissionMiddleware:
+class RolepermissionMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
